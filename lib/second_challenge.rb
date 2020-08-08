c593677cf50741e56895288bca9aca0ebe67d98a
Javier Aguilar <coyote176@msn.com>
2020-08-08 18:55:46 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -18,14 +18,3 @@ def second_challenge
   }
 end
   
-epic_tragedy = {
-    :montague => {
-      #your key/value pairs here!
-      :patriarch =>{},
-      :matriarch =>{},
-      :hero =>{},
-      :hero_friends =>[]
-      }, 
-    :capulet => {
-      #your key/value pairs here!
-      }}
