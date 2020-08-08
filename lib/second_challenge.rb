8bc4d581f493817b611665aca22967236342b613
Javier Aguilar <coyote176@msn.com>
2020-08-08 18:55:33 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -1,21 +1,31 @@
 def second_challenge
   epic_tragedy = {
     :montague => {
-      :patriarch => {},
-      :matriarch => {},
-      :hero => {},
-      :hero_firends => []
+      :patriarch =>{},
+      :matriarch =>{},
+      :hero =>{},
+      :hero_firends =>[]
       #your key/value pairs here!
       }, 
     :capulet => {
-      :patriarch => {},
-      :matriarch => {},
-      :heroine => {},
-      :heroine_friends => []
+      :patriarch =>{},
+      :matriarch =>{},
+      :heroine =>{},
+      :heroine_friends =>[]
       #your key/value pairs here!
       }
     
   }
 end
   
-
+epic_tragedy = {
+    :montague => {
+      #your key/value pairs here!
+      :patriarch =>{},
+      :matriarch =>{},
+      :hero =>{},
+      :hero_friends =>[]
+      }, 
+    :capulet => {
+      #your key/value pairs here!
+      }}
